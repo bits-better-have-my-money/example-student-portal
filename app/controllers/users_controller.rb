c65a89a7d4cab6d75e41b69cd36a47cc3aaafd56
dani <dani.zaghian@gmail.com>
2021-10-18 09:21:27 -0700

    adds limit and offset to users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
   def index
-    users = User.where(role_id: 1)
+    users = User.all
+
+    if params[:role_id]
+      users = users.where("role_id = ?", params[:role_id])
+    end
+
+    if params[:cohort_id]
+      users = users.where("cohort_id = ?", params[:cohort_id])
+    end
+
+    if params[:limit]
+      users = users.limit(params[:limit])
+    end
+
+    if params[:offset]
+      users = users.offset(params[:offset])
+    end
+
+    users = users.order(:last_name)
+
     render json: users
   end
 end
