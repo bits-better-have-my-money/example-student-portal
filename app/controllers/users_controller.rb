c998a4882f9c59391cb294b298f8819a21669d65
dani <dani.zaghian@gmail.com>
2021-10-18 09:48:56 -0700

    simplifies users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,29 @@
 class UsersController < ApplicationController
   def index
-    users = User.all
-
-    if params[:role_id]
-      users = users.where("role_id = ?", params[:role_id])
-    end
-
-    if params[:cohort_id]
-      users = users.where("cohort_id = ?", params[:cohort_id])
-    end
-
-    if params[:limit]
-      users = users.limit(params[:limit])
-    end
-
-    if params[:offset]
-      users = users.offset(params[:offset])
-    end
-
-    users = users.order(:last_name)
-
+    users = User.where(role_id: 1)
+      .limit(params[:limit])
+      .offset(params[:offset])
     render json: users
   end
 end
+
+
+
+
+# if params[:role_id]
+  #   users = users.where("role_id = ?", params[:role_id])
+  # end
+
+  # if params[:cohort_id]
+  #   users = users.where("cohort_id = ?", params[:cohort_id])
+  # end
+
+  # if params[:limit]
+  #   users = users.limit(params[:limit])
+  # end
+
+  # if params[:offset]
+  #   users = users.offset(params[:offset])
+  # end
+
+  # users = users.order(:last_name)
